ab2d4babe18357efc68a2976d414cd61cf73f86e
Maxim Orlov <orlovmaxims@gmail.com>
2020-07-27 22:06:27 +0300

    Initial commit

---
+++ app/src/main/java/com/android/pizzarecipes/Utils.java
@@ -0,0 +1,75 @@
+package com.android.pizzarecipes;
+
+public class Utils {
+
+    public static final String PIZZA_1_TITLE = "Next level Margherita pizza";
+    public static final String PIZZA_1_DESCRIPTION = "Forget takeaways – you can’t beat a homemade Margherita pizza topped with fresh tomato sauce and melted cheese. Here's how to master this everyday classic...";
+    public static final String PIZZA_1_RECIPE = "First, make the dough. Tip the flour into a bowl and add 300ml tepid water. Mix together and set aside at room temperature for 1 hr. Dissolve the yeast in 2 tbsp water and mix this and 15g of salt through the dough. Cover with cling film and leave somewhere warm to double in size for a few hours. For a sourer flavour, leave in the fridge for at least 8 hrs and up to 24 hrs – the longer you leave it the sourer it will be.\n" +
+            "\n" +
+            "    When the dough is ready, tip it onto a lightly floured surface and divide into four. Roll into balls and leave to rest, covered with a tea towel or cling film for another hour.\n" +
+            "\n" +
+            "    Now make the tomato sauce. Drain some of the juice from the can and tip the rest into a bowl with the olive oil, oregano and a generous pinch of salt, then either scrunch everything together with your fingers for a chunky sauce or blitz with a stick blender if you want it smooth. Tie the basil stalks together, bruise with the back of a knife and place in the sauce. Leave the sauce at room temperature until needed.\n" +
+            "\n" +
+            "    To make the pizza, heat a grill to its highest setting and get a heavy frying pan. On a floured surface push and stretch one of the balls of dough out into a circle roughly the same size as the frying pan. Slip the round onto a floured baking sheet and top with a quarter of the sauce, a scattering of cheese, a few basil leaves and a quarter of the mozzarella.\n" +
+            "\n" +
+            "    Get the pan very hot and carefully slide the pizza onto it. Cook for 2 mins, then put the pan under the grill for another 2 mins until the sides are puffed up and the cheese has melted. Lift onto a board, drizzle with a little olive oil if you like, then cut into wedges and serve while you make the next one.";
+    public static final String PIZZA_2_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_2_DESCRIPTION = "Even a novice cook can master the art of pizza with our simple step-by-step guide. Bellissimo";
+    public static final String PIZZA_2_RECIPE = "Make the base: Put the flour into a large bowl, then stir in the yeast and salt. Make a well, pour in 200ml warm water and the olive oil and bring together with a wooden spoon until you have a soft, fairly wet dough. Turn onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside. You can leave the dough to rise if you like, but it’s not essential for a thin crust.\n" +
+            "\n" +
+            "    Make the sauce: Mix the passata, basil and crushed garlic together, then season to taste. Leave to stand at room temperature while you get on with shaping the base.\n" +
+            "\n" +
+            "    Roll out the dough: If you’ve let the dough rise, give it a quick knead, then split into two balls. On a floured surface, roll out the dough into large rounds, about 25cm across, using a rolling pin. The dough needs to be very thin as it will rise in the oven. Lift the rounds onto two floured baking sheets.\n" +
+            "\n" +
+            "    Top and bake: Heat oven to 240C/fan 220C /gas 8. Put another baking sheet or an upturned baking tray in the oven on the top shelf. Smooth sauce over bases with the back of a spoon. Scatter with cheese and tomatoes, drizzle with olive oil and season. Put one pizza, still on its baking sheet, on top of the preheated sheet or tray. Bake for 8-10 mins until crisp. Serve with a little more olive oil, and basil leaves if using. Repeat step for remaining pizza.";
+    public static final String PIZZA_3_TITLE = "Caramelised onion & goat's cheese pizza ";
+    public static final String PIZZA_3_DESCRIPTION = "Make a veggie pizza that's healthy and low-fat instead of ordering a takeaway. This easy recipe uses goat's cheese for tang while the onions give it sweetness...";
+    public static final String PIZZA_3_RECIPE = "Heat oven to 220C/200C fan/gas 7. Tip the flour into a mixer with a dough hook, or a bowl. Add the yeast, salt, oil and just under 100ml warm water then mix to a soft dough. Knead in the food mixer for about 5 mins, but if making this by hand, tip onto a work surface and knead for about 10 mins. The dough is sticky, but try not to add too much extra flour. Leave in the bowl and cover with a tea towel while you make the topping. There is no need to let the dough prove for a specific time – just let it sit while you get on with the next step.\n" +
+            "\n" +
+            "    Tip the onions into a non-stick wok and add the oil, 4 tbsp water and balsamic vinegar. Cover with a saucepan lid that sits inside the pan to help the onions soften, then cook for 15 mins, stirring about 3 times and replacing the lid quickly so as not to lose too much moisture. After the time is up, the onions should be golden and all the liquid gone. Tip onto a plate. Add the spinach and garlic to the pan and stir-fry until the spinach has wilted.\n" +
+            "\n" +
+            "    Take the dough from the bowl and cut in half with an oiled knife, then press each piece into a 25-15 cm oval on a large greased baking sheet with oiled hands. Don’t knead the dough first otherwise it will be too elastic and it will keep shrinking back.\n" +
+            "\n" +
+            "    Spread with the spinach followed by the onions, then dot with the cheese and scatter with the olives, thyme and sunflower seeds. Bake for 15 mins until golden and the base is cooked through.";
+    public static final String PIZZA_4_TITLE = "Superhealthy pizza";
+    public static final String PIZZA_4_DESCRIPTION = "The quantities for this are generous, so if you have any leftovers, pop a wedge of cold pizza into your lunchbox the next day";
+    public static final String PIZZA_4_RECIPE = "Mix the flours and yeast with a pinch of salt in a food processor fitted with a dough blade. Pour in the water and mix to a soft dough, then work for 1 min. Remove the dough and roll out on a lightly floured surface to a round about 30cm across. Lift onto an oiled baking sheet.\n" +
+            "\n" +
+            "    Spread the canned tomatoes over the dough to within 2cm of the edges. Arrange the cherry tomatoes and courgettes over the top, then scatter with the mozzarella. Mix the capers, olives and garlic, then scatter over the top. Drizzle evenly with the oil. Leave to rise for 20 mins. Heat oven to 240C/ fan 220C/gas 9 or the highest setting.\n" +
+            "\n" +
+            "    Bake the pizza for 10-12 mins until crisp and golden around the edges. Scatter with the parsley to serve.";
+    public static final String PIZZA_5_TITLE = "Ultimate pizza Margherita";
+    public static final String PIZZA_5_DESCRIPTION = "Take your senses on a trip to Italy with this authentic pizza Margherita";
+    public static final String PIZZA_5_RECIPE = "In a bowl, mix flour, semolina, salt and yeast. Make a dip in the middle, pour in oil, water, then stir and squeeze everything together (hands are easiest for this). The dough should feel very sticky (pic 1). When it is well mixed, cover and leave for 15 mins.\n" +
+            "\n" +
+            "    Tip the dough on to a lightly oiled work surface and rub your hands and the inside of the bowl with oil. Knead the dough about 12 times only, giving it a quarter turn each time you knead. Tuck the ends under so it is the shape of a ball and lay it in the bowl, seam side down. Cover and leave for 10 mins. Repeat the kneading and leaving for 10 mins again, then knead and leave for 15 mins, oiling the surface and your hands each time.\n" +
+            "\n" +
+            "    In between kneadings make the sauce for the topping. Drain the tomatoes and tip them into a bowl. Snip them into small pieces with scissors (pic 2). Stir in garlic, tomato purée, and seasoning. Set aside. Brush a baking sheet with oil and sprinkle with a little semolina. Heat oven to 240C/fan 220C/gas 9.\n" +
+            "\n" +
+            "    When the dough is ready, cut in half and put one half onto a lightly fl oured surface. Knead 4-5 times to squash out any air bubbles, then roll it out. At the point where it just keeps springing back, brush off the flour from the work surface and rub on a little oil. This makes it easier to roll. Continue to roll out until you have a 28cm circle, pulling it into shape as well. Lift it onto the baking sheet (it’s easier if you drape it over a rolling pin).\n" +
+            "\n" +
+            "    Drain the mozzarella, then pat dry with kitchen paper. Brush the dough with olive oil, then spread half the sauce over, almost up to the edge. Scatter over half the basil, then tear up one mozzarella ball and scatter over the basil (pic 3). Grate over pepper and drizzle with a little oil. Bake for 12-15 mins, until the topping is bubbling and the dough is going brown. Repeat with the remaining dough and toppings. To serve, scatter with parmesan shavings, rocket leaves and drizzle with oil.";
+    public static final String PIZZA_6_TITLE = "Pizza with homemade sauce";
+    public static final String PIZZA_6_DESCRIPTION = "Make pizza for the family with a homemade base and tomato sauce. The recipe is perfect to get kids involved in cooking. Top with mozzarella and fresh basil";
+    public static final String PIZZA_6_RECIPE = "Tip the flour into a bowl, then stir in the yeast and 1 tsp salt. Make a well in the centre and pour in 200ml warm water (make sure it’s not too hot) along with the oil. Stir together with a wooden spoon until you have a soft, fairly wet dough.\n" +
+            "\n" +
+            "Tip the dough out onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside for an hour or so or until the dough has puffed up and doubled in size. You can also leave the rough, unkneaded dough in the bowl, cover with a tea towel and leave in the fridge overnight and the dough will continue to prove on its own.\n" +
+            "\n" +
+            "Meanwhile, make the tomato sauce. Put the oil in a small pan and fry the garlic briefly (don’t let it brown), then add the passata and simmer everything until the sauce thickens a little. Leave to cool.\n" +
+            "\n" +
+            "Once the dough has risen, knead it quickly in the bowl to knock it back, then tip out onto a lightly floured surface and cut into two balls. Roll out each ball into a large teardrop that is very thin and about 25cm across (teardrop shapes fit baking sheets more easily than rounds). \n" +
+            "\n" +
+            "Heat oven to 240C/220C fan/ gas 9 with a large baking sheet inside. Lift one of the bases onto another floured baking sheet. Smooth the sauce over the base with the back of a spoon, scatter over half the mozzarella, drizzle with olive oil and season. Put the pizza, still on its baking sheet, on top of the hot sheet in the oven and bake for 8-10 mins until crisp.";
+    public static final String PIZZA_7_TITLE = "Rainbow pizzas";
+    public static final String PIZZA_7_DESCRIPTION = "Forget takeaway pizzas, encourage kids to eat a rainbow every day with this colourful recipe full of fresh flavours – guaranteed to please even fussy eaters...";
+    public static final String PIZZA_7_RECIPE = "Heat the oven to 180C/160C fan/gas 4. Put each pizza base on a baking sheet and spread each with half of the passata. Arrange the tomatoes on the top in rings or wedges of colour and add the broccoli and the olives, if using. Squish the mozzarella cherries (bocconcini) a little before dotting them over the pizzas, then drizzle 1 tbsp pesto over each.\n" +
+            "\n" +
+            "    Bake for 15-20 mins or until the top is bubbling and just starting to brown a little. Scatter over the basil leaves before serving.";
+    public static final String PIZZA_8_TITLE = "Pork, gorgonzola & garlic butter pizza";
+    public static final String PIZZA_8_DESCRIPTION = "Garlic butter, salty blue cheese, peppery rocket and a kick of chilli – these pizzas are loaded with flavour. Try making them for an indulgent weekend dinner...";
+    public static final String PIZZA_8_RECIPE = "To make the dough, put the flour, yeast and 1 tsp salt into the bowl of an electric mixer with a dough hook. On a medium speed, gradually pour in the oil and 200ml warm water to make a dough. Alternatively, mix by hand and knead on a floured surface. Cover the bowl with cling film and prove for 1 hr or until doubled in size.\n" +
+            "\n" +
+            "    Meanwhile, fry the mince until browned, then drain on a kitchen towel. Make the garlic butter by beating the butter with the garlic, sage, tomato purée and some black pepper.\n" +
+            "\n" +
+            "    Heat the oven to 240C/220C fan/gas 8. Divide the dough into four balls. Roll the balls on a floured surface into thin rounds, about 20cm wide. Lift onto floured baking trays, then spread with the garlic butter, leaving a 1cm border. Sprinkle with the mince, cheese, onion and chilli. Bake for 10-12 mins or until the dough is crisp. Scatter over the rocket, olives and lemon zest, and drizzle over some olive oil to serve.";
+}
